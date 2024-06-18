54985e481d8dbb514153fab0a9b696b48c8e543a
Chaemin Yooun <yooune.01@gmail.com>
2024-06-18 22:03:44 +0900

    Initial commit

---
+++ src/main/java/pupket/togedogserver/TogedogServerApplication.java
@@ -0,0 +1,13 @@
+package pupket.togedogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogedogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogedogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pupket/togedogserver/TogedogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pupket.togedogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogedogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
