32e502a68305ec9df8fe523e3f60edd9610c3df1
Chaemin Yooun <cinnamein@gmail.com>
2024-10-07 07:32:22 +0900

    socketio hostname 변경

--- src/main/java/pupket/togedogserver/global/socketio/config/SocketIOConfig.java
+++ src/main/java/pupket/togedogserver/global/socketio/config/SocketIOConfig.java
@@ -10,7 +10,7 @@ public class SocketIOConfig {
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        config.setHostname("localhost");
+        config.setHostname("togedog.life");
         config.setPort(9092);
         return new SocketIOServer(config);
     }
