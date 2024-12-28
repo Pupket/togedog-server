547437de5d834812bd726d6fc104cc8a0b493001
Sunro1994 <gwangjulsr@gmail.com>
2024-12-28 17:52:55 +0900

    예외 처리중 필요없는 코드 제거

--- src/main/java/pupket/togedogserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/pupket/togedogserver/global/exception/GlobalExceptionHandler.java
@@ -23,10 +23,6 @@ public class GlobalExceptionHandler {
 
         ExceptionResponse exceptionResponse = ExceptionResponse.fromException(ex.getExceptionCode());
 
-        if (exceptionResponse.httpStatus().equals(HttpStatus.CONFLICT)) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exceptionResponse);
-        }
-
         return ResponseEntity.status(exceptionResponse.httpStatus()).body(exceptionResponse);
     }
 
